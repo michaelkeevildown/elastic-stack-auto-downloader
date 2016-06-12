9910ae958e663b02af64698ef8d8bb3d830cc59d
Michael Down <down@elastic.co>
2016-06-12 20:41:08 +0100

    changed file permissions from 777 to 755

--- move.sh
+++ move.sh
@@ -2,5 +2,5 @@ echo "Moving to local bin"
 echo "sudo cp elastic-downloader.py /usr/local/bin/elastic-downloader"
 sudo cp elastic-downloader.py /usr/local/bin/elastic-downloader
 echo "sudo chmod 777 /usr/local/bin/elastic-downloader"
-sudo chmod 777 /usr/local/bin/elastic-downloader
+sudo chmod 755 /usr/local/bin/elastic-downloader
 echo "Done..."
