fb3076d460dcad3e7cfe6d7a9c8ff639861db505
Michael Down <down@elastic.co>
2016-06-12 18:35:43 +0100

    create simple bash command to automate the move of the python script into my local bin folder

---
+++ move.sh
@@ -0,0 +1,6 @@
+echo "Moving to local bin"
+echo "sudo cp elastic-downloader.py /usr/local/bin/elastic-downloader"
+sudo cp elastic-downloader.py /usr/local/bin/elastic-downloader
+echo "sudo chmod 777 /usr/local/bin/elastic-downloader"
+sudo chmod 777 /usr/local/bin/elastic-downloader
+echo "Done..."
